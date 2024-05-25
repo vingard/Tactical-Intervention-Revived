e9ff65caee3019514a5e4eb197e6d0fa4f40b61b
Jake <commits.vin@gmail.com>
2024-05-25 14:22:31 +0100

    experimental streamed srcds

--- src/main/ds/main.ts
+++ src/main/ds/main.ts
@@ -1,12 +1,19 @@
 import { program } from "commander";
 import { app } from "electron";
 import { hideBin } from "yargs/helpers";
+import log from "electron-log"
+
+import * as server from "../core/server"
+import * as game from "../core/game"
+
 
 export async function serverInit() {
     program
         .requiredOption("-p, --port <number>", "Port to host the server on", (x) => parseInt(x, 10), 27015)
 
     program.parse(hideBin(process.argv))
+
+    if (!game.isInstalled) return log.warn("Tried to start dedicated server when game was not installed!")
 
     const allOptions: any = {}
 
@@ -17,4 +24,6 @@ export async function serverInit() {
     //console.table(allOptions)
 
     console.log(`Tactical Intervention Revived Dedicated Server (${app.getVersion()}) started on port ${program.getOptionValue("port")}`)
+
+    server.startExperimentalStreamed()
 }
